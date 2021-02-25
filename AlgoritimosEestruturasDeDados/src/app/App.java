22e8be32af3e2418cbd9bbd2ba395cd212a7462b
jessicammelo <jessica.08041992@gmail.com>
2021-02-24 22:01:58 -0300

    no message

---
+++ AlgoritimosEestruturasDeDados/src/app/App.java
@@ -0,0 +1,28 @@
+package app;
+
+import listas.ListaEstatica;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ListaEstatica le = new ListaEstatica();
+		le.inserir(40);
+		le.inserir(46);
+		le.inserir(46);
+		le.inserir(46);
+		le.inserir(46);
+		le.inserir(46);
+		le.inserir(46);
+		le.inserir(46);
+		le.inserir(46);
+		le.inserir(46);
+		le.inserir(46);
+		le.inserir(46);
+		le.inserir(46);
+		
+	System.out.println("funcjauiaigfsg");
+	}
+
+}
---
+++ AlgoritimosEestruturasDeDados/src/listas/ListaEstatica.java
@@ -0,0 +1,34 @@
+package listas;
+
+public class ListaEstatica {
+
+	private int info[];
+
+	private int tamanho;
+
+	public ListaEstatica() {
+		info = new int[10];
+		tamanho = 0;
+	}
+
+	public void inserir(int valor) {
+		if (tamanho == info.length) {
+			redimencionar();
+		}
+		info[tamanho] = valor;
+		tamanho = tamanho + 1;
+	}
+
+	public void redimencionar() {
+		int novo[];
+		int novotamanho = info.length + 10;
+		novo = new int[novotamanho];
+		for (int i = 0; i < tamanho -1;i++) {
+			novo[i] = info[i];
+		}
+		info = novo;
+	}
+	
+	
+
+}
