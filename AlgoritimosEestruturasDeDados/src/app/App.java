40d1ee557a2697cc6dbb07ca8071226a0614427a
jessicammelo <jessica.08041992@gmail.com>
2021-03-07 21:07:54 -0300

    commit

--- AlgoritimosEestruturasDeDados/src/app/App.java
+++ AlgoritimosEestruturasDeDados/src/app/App.java
@@ -8,21 +8,88 @@ public class App {
 		// TODO Auto-generated method stub
 
 		ListaEstatica le = new ListaEstatica();
-		le.inserir(40);
-		le.inserir(46);
-		le.inserir(46);
-		le.inserir(46);
-		le.inserir(46);
-		le.inserir(46);
-		le.inserir(46);
-		le.inserir(46);
-		le.inserir(46);
-		le.inserir(46);
-		le.inserir(46);
-		le.inserir(46);
-		le.inserir(46);
-		
-	System.out.println(le);
-	}
+		le.inserir(5);
+		le.inserir(10);
+		le.inserir(15);
+		le.inserir(20);
 
+		System.out.println(le.toString());
+		System.out.println(le.getTamanho());
+		System.out.println(le.buscar(15));
+		System.out.println(le.buscar(30));
+		le.retirar(10);
+		System.out.println(le.toString());
+		System.out.println(le.getTamanho());
+
+		ListaEstatica li = new ListaEstatica();
+//	li.inserir(1);
+//	li.inserir(2);
+//	li.inserir(3);
+//	li.inserir(4);
+		li.inserir(5);
+//	li.inserir(6);
+//	li.inserir(7);
+//	li.inserir(8);
+//	li.inserir(9);
+		li.inserir(10);
+//	li.inserir(11);
+//	li.inserir(12);
+//	li.inserir(13);
+//	li.inserir(14);
+		li.inserir(15);
+		li.inserir(20);
+
+//	System.out.println(li.toString());
+//	System.out.println(li.getTamanho());
+//	System.out.println(li.obterElemento(3));
+		// System.out.println(li.obterElemento(5));
+		// li.liberar();
+		// System.out.println(li.estaVazia());
+
+		lista02.ListaEstatica<Integer> testes = new lista02.ListaEstatica<>();
+
+		// Teste teste = new Teste();
+		// teste.setNome("João");
+		// teste.setEndereco("Rua sao paulo");
+
+//		testes.inserir(5);
+//		testes.inserir(10);
+//		testes.inserir(15);
+//		testes.inserir(20);
+//		testes.inverter();
+//
+//		System.out.println(testes.toString());
+
+		testes.inserir(5);
+		testes.inserir(10);
+		testes.inserir(15);
+		testes.inserir(20);
+		testes.inserir(25);
+		
+		testes.inverter();
+		
+		System.out.println(testes.toString());
+
+//	testes.inserir(teste);
+//	
+//	Teste teste2 = new Teste();
+//	teste2.setNome("ana");
+//	
+//	testes.inserir(teste2);
+//	
+//	Teste teste3 = new Teste();
+//	teste3.setNome("Marco");
+//	
+//	testes.inserir(teste3);
+//	testes.exibir();
+//	testes.inverter();
+//	testes.exibir();
+//	System.out.println(testes.obterElemento(0).getNome());
+//	testes.inverter();
+//	System.out.println(testes.obterElemento(0).getNome());
+//		
+//	}
+//	
+//	
+	}
 }
---
+++ AlgoritimosEestruturasDeDados/src/lista02/ListaEstatica.java
@@ -0,0 +1,99 @@
+package lista02;
+
+public class ListaEstatica <ClasseParametrizada> {
+
+	private ClasseParametrizada[] info;
+	private int tamanho;
+	
+	@SuppressWarnings("unchecked")
+	public ListaEstatica() {	
+		info = (ClasseParametrizada[]) new Object[10];
+		tamanho = 0;
+	}
+	
+	private void redimensionar() {
+		ClasseParametrizada novo[];
+		int novoTamanho = info.length + 10;
+		novo = (ClasseParametrizada[]) new Object[novoTamanho];
+		for (int i = 0; i < tamanho - 1; i++) {
+			novo[i] = info[i];
+		}
+		info = novo;
+	}
+	
+	public void inserir(ClasseParametrizada valor) {
+		if (tamanho == info.length) {
+			redimensionar();
+		}
+		info[tamanho] = valor;
+		tamanho++;
+	}
+	
+	public void exibir() {
+		for (int i = 0; i < tamanho; i++) {
+			System.out.println(info[i]);
+		}
+	}
+	
+	public int buscar(ClasseParametrizada valor) {
+		for (int i = 0; i < tamanho; i++) {
+			if (info[i] == valor) {
+				return i;
+			}
+		}
+		return -1;
+			
+	}
+	
+	public void retirar(ClasseParametrizada valor) {
+		int posicao = buscar(valor);
+		if (posicao > -1) {
+			for (int i = posicao; i < tamanho - 1; i++) {
+				info[i] = info[i + 1];
+			}
+			tamanho--;
+		}
+	}
+	
+	public void liberar() {
+		info = (ClasseParametrizada[]) new Object[10];;
+		tamanho = 0;
+	}
+	
+	public ClasseParametrizada obterElemento(int posicao) {
+		if (posicao >= 0 && (posicao < tamanho)) {
+			return info[posicao];
+		} else {
+			throw new IndexOutOfBoundsException();
+		}
+	}
+	
+	public boolean estaVazia() {
+		return tamanho == 0;
+	}
+	
+	public int getTamanho() {
+		return tamanho;
+	}
+	
+	public String toString() {
+		String resultado = "";
+		for ( int i = 0; i< tamanho; i++) {
+			if ( i>0)
+				resultado += ",";
+			resultado += info [i];
+		}
+		return resultado;
+	}
+	
+	public void inverter() {
+		for (int i = 0; i < tamanho / 2; i++) {
+			ClasseParametrizada p = info[i];
+			info[i] = info[tamanho - i - 1];
+			info[tamanho - i - 1] = p;
+		}
+	}
+ 
+	
+	
+}
